fae1702841d384a12fd1c5853187fef9ee26f6ee
Victor Cordova <victor.cordova@travelperk.com>
2020-05-19 18:00:47 +0200

    feat: make routes match with exact paths and make list recipes default route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,10 @@
 import React from "react"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom"
 
 import { NavBar } from "./components"
 import { CreateRecipe, ListRecipes, URLS } from "../recipes"
@@ -9,12 +14,13 @@ const App = () => (
     <Router>
       <NavBar />
       <Switch>
-        <Route path={URLS.CREATE}>
+        <Route exact path={URLS.CREATE}>
           <CreateRecipe />
         </Route>
-        <Route path={URLS.LIST}>
+        <Route exact path={URLS.LIST}>
           <ListRecipes />
         </Route>
+        <Redirect to={URLS.LIST} />
       </Switch>
     </Router>
   </div>
