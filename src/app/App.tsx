af92608b29933712efcd6ece981dda60cb213b56
Victor Cordova <victor.cordova@travelperk.com>
2020-05-20 16:56:56 +0200

    refactor(app): expose single point of entry from app and export main container

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,7 +9,7 @@ import {
 import { NavBar } from "./components"
 import { CreateRecipe, ListRecipes, EditRecipe, URLS } from "../recipes"
 
-const App = () => (
+export const App = () => (
   <div>
     <Router>
       <NavBar />
@@ -28,5 +28,3 @@ const App = () => (
     </Router>
   </div>
 )
-
-export default App
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { createGlobalStyle } from "styled-components"
 import { normalize } from "styled-normalize"
-import App from "./app/App"
+import { App } from "./app/App"
 import * as serviceWorker from "./serviceWorker"
 
 const GlobalStyle = createGlobalStyle`
